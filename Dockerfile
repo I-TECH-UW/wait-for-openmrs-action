4ed7ba7a0e9e2cd40a1b543f9e2c856f5eae07a7
pmanko <pmanko@uw.edu>
2022-01-21 14:22:30 -0800

    Action Dockerfile fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM alpine:latest
+FROM bash:latest
 
 COPY entrypoint.sh /entrypoint.sh
 
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENV TARGET_URL="http://isanteplus:8080/openmrs"
+ENV INTERVAL=10
+ENV TIMEOUT=100
+
+RUN apk add --update-cache \
+    curl \
+  && rm -rf /var/cache/apk/*
+  
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["${TARGET_URL}", "${INTERVAL}", "${TIMEOUT}"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,41 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
+url=${1:-"http://isanteplus:8080/openmrs"}
+interval=${2:-10}
+timeout=${3:-100}
 
-url=$1
-interval=$2
-timeout=$3
-
-TIMEOUT_END=$(($(date +%s) + $timeout))
+START_TIME=$(date +%s)
+TIMEOUT_END=$(($START_TIME + $timeout))
 
 
 while :; do
-    echo "Waiting for $1"
+    echo "Waiting for $url"
 
     response=$(curl -u admin:Admin123 -s -w "\n%{http_code}" $url/ws/fhir2/R4/metadata?_format=json)
     response=(${response[@]}) # convert to array
     code=${response[-1]} # get last element (last line)
 
-    echo $code
+    echo "Response Code: $code"
 
     body=${response[@]::${#response[@]}-1} # get all elements except last
 
     if [[ "${body[0]}" == *"CapabilityStatement"* ]]; then
-        echo "Got Metadata!"
+        echo "Got Metadata after $(($(date +%s)-$START_TIME)) seconds!"
+        exit 0
+    else
+        echo "Still waiting..."
+    fi
+    
+    sleep $interval
+    response=$(curl -u admin:Admin123 -s -w "\n%{http_code}" $url/ws/rest/v1/session)
+    response=(${response[@]}) # convert to array
+
+    code=${response[-1]} # get last element (last line)
+
+    echo "Response Code: $code"
+    
+    if [[ "$code" == "200" ]]; then
+        echo "Got API after $(($(date +%s)-$START_TIME)) seconds!"
         exit 0
     else
         echo "Still waiting..."
